09fdfca6a357ca16d9be4957b0983fe75caa0f1d
aptheparker <apapap1119@g.skku.edu>
2024-11-23 00:26:24 +0900

    viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
