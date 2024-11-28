e0e62f83188e4ece9756dcdf42c69abdcaa3f24e
aptheparker <apapap1119@g.skku.edu>
2024-11-28 22:20:03 +0900

    feat: init background

--- src/components/home/InitScreen.tsx
+++ src/components/home/InitScreen.tsx
@@ -1,6 +1,10 @@
-import Image from 'next/image';
-import InitScreenImage from '/public/img/init_screen.png';
-
 export default function InitScreen() {
-  return <Image src={InitScreenImage} alt="init screen image" />;
+  return (
+    <div className="video-background">
+      <video autoPlay loop muted playsInline>
+        <source src="/video/init_screen.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
 }
